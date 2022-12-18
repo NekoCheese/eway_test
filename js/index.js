a61bc9ab5b88d47a99a564f361b6f1b1574d0b34
NekoCheese <happaite4664@gmail.com>
2022-12-18 16:20:27 +0800

    1218_修改表單樣式

--- js/index.js
+++ js/index.js
@@ -40,7 +40,7 @@ new Vue({
       const nameExp = /^[\u4e00-\u9fa5a-zA-Z]+$/;
       if (!nameExp.test(this.f_name)) {
         this.f_name_error = true;
-        this.f_name_ErrMsg = "Please enter Chinese or English";
+        this.f_name_ErrMsg = "required";
       } else {
         this.f_name_error = false;
         this.f_name_ErrMsg = "";
@@ -52,7 +52,7 @@ new Vue({
       const phoneExp = /^09\d{8}$/;
       if (!phoneExp.test(this.f_phone)) {
         this.f_phone_error = true;
-        this.f_phone_ErrMsg = "Please enter the correct phone number";
+        this.f_phone_ErrMsg = "required";
       } else {
         this.f_phone_error = false;
         this.f_phone_ErrMsg = "";
@@ -64,7 +64,7 @@ new Vue({
       const amountExp = /^[0-9]+$/;
       if (!amountExp.test(this.f_amount)) {
         this.f_amount_error = true;
-        this.f_amount_ErrMsg = "Wrong amount entered";
+        this.f_amount_ErrMsg = "required";
       } else {
         this.f_amount_error = false;
         this.f_amount_ErrMsg = "";
@@ -73,7 +73,7 @@ new Vue({
 
     f_store: function () {
       if (this.f_store == "") {
-        this.f_store_ErrMsg = "Please select a store from the list";
+        this.f_store_ErrMsg = "required";
       } else {
         this.f_store_error = false;
         this.f_store_ErrMsg = "";
@@ -82,7 +82,7 @@ new Vue({
 
     f_payment: function () {
       if (this.f_payment == "digital payment") {
-        this.f_payment_ErrMsg = "請選擇付款方式";
+        this.f_payment_ErrMsg = "required";
       } else {
         this.f_payment_error = false;
       }
@@ -101,7 +101,9 @@ new Vue({
       ) {
         alert("Data successfully sent");
       } else {
-        alert("Failed to send, please confirm that the information is filled in correctly");
+        alert(
+          "Failed to send, please confirm that the information is filled in correctly"
+        );
       }
     },
 
